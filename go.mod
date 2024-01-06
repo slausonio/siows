ca8e95a04167caae715bb6e64b9a489b4fa2ae59
Matthew Slauson <matthew@slauson.io>
2024-01-06 14:43:29 -0600

    start

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/slausonio/go-webserver
 
 go 1.21.5
 
-require github.com/google/uuid v1.5.0
+require (
+	github.com/google/uuid v1.5.0
+	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/slausonio/siogo v0.0.0-20240106202559-c1cf88c18ef7
+)
+
+require (
+	golang.org/x/exp v0.0.0-20240103183307-be819d1f06fc // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ server/environment.go
@@ -0,0 +1,84 @@
+package server
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"github.com/slausonio/siogo"
+)
+
+const (
+	AppName = "APP_NAME"
+	Env     = "ENV"
+	Port    = "PORT"
+)
+
+var (
+	ErrNoEnvFile = errors.New("no .env file found in root of project")
+	ErrNoAppName = errors.New("no APP_NAME env var found")
+	ErrNoEnv     = errors.New("no ENV env var found")
+)
+
+type Environment map[string]string
+
+func (e Environment) Value(key string) string {
+	return e[key]
+}
+
+func (e Environment) Update(key, value string) {
+	e[key] = value
+}
+
+func NewEnvironment() Environment {
+	environment := make(Environment)
+
+	return environment
+}
+
+func (e Environment) readEnvironment() Environment {
+	defaultEnvFile := readDefaultEnvFile()
+	mergedEnv := siogo.MergeMaps(defaultEnvFile, e)
+
+	environment := make(Environment)
+	environment["PATH"] = "/bin:/usr/bin"
+	return environment
+}
+
+func readDefaultEnvFile() Environment {
+	defaultEnvFile, err := godotenv.Read()
+	if err != nil {
+		dotEnvErr := fmt.Errorf("dot env err: %w", err)
+		logrus.Error(dotEnvErr)
+
+		panic(ErrNoEnvFile)
+	}
+
+	return defaultEnvFile
+}
+
+func readEnv() string {
+	appName, ok := os.LookupEnv(Env)
+	if !ok {
+		err := fmt.Errorf("new environment: %w", ErrNoEnv)
+
+		logrus.Error(err)
+		panic(err)
+	}
+
+	return appName
+}
+
+func readAppName() string {
+	appName, ok := os.LookupEnv(AppName)
+	if !ok {
+		err := fmt.Errorf("new environment: %w", ErrNoAppName)
+
+		logrus.Error(err)
+		panic(err)
+	}
+
+	return appName
+}
