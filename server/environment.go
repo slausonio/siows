e5dc55ca185670459c0e6d1739d1ed661066a7da
Matthew Slauson <matthew@slauson.io>
2024-01-06 22:44:09 -0600

    finish environment

--- server/environment.go
+++ server/environment.go
@@ -11,15 +11,15 @@ import (
 )
 
 const (
-	AppName = "APP_NAME"
-	Env     = "ENV"
-	Port    = "PORT"
+	AppName    = "APP_NAME"
+	CurrentEnv = "ENV"
+	Port       = "PORT"
 )
 
 var (
-	ErrNoEnvFile = errors.New("no .env file found in root of project")
-	ErrNoAppName = errors.New("no APP_NAME env var found")
-	ErrNoEnv     = errors.New("no ENV env var found")
+	ErrNoEnvFile    = errors.New("no .env file found in root of project")
+	ErrNoAppName    = errors.New("no APP_NAME env var found")
+	ErrNocurrentEnv = errors.New("no ENV env var found")
 )
 
 type Environment map[string]string
@@ -33,9 +33,11 @@ func (e Environment) Update(key, value string) {
 }
 
 func NewEnvironment() Environment {
-	environment := make(Environment)
+	env := make(Environment)
+	env = env.readEnvironment()
+	env.setEnvToSystem()
 
-	return environment
+	return env
 }
 
 func (e Environment) readEnvironment() Environment {
@@ -43,17 +45,23 @@ func (e Environment) readEnvironment() Environment {
 	defaultEnvMap := readDefaultEnvFile()
 	currentEnvMap := readEnvironmentSpecificEnvFile(currentEnv)
 
-	mergedEnv := siogo.MergeMaps(defaultEnvMap, currentEnv)
+	mergedEnv := siogo.MergeMaps(defaultEnvMap, currentEnvMap)
 
 	return mergedEnv
+}
+
+func (e Environment) setEnvToSystem() {
+	for key, value := range e {
+		os.Setenv(key, value)
+	}
 }
 
 func readDefaultEnvFile() Environment {
 	defaultEnvFile, err := godotenv.Read()
 	if err != nil {
 		dotEnvErr := fmt.Errorf("dot env err: %w", err)
-		logrus.Error(dotEnvErr)
 
+		logrus.Error(dotEnvErr)
 		panic(ErrNoEnvFile)
 	}
 
@@ -62,6 +70,7 @@ func readDefaultEnvFile() Environment {
 
 func readEnvironmentSpecificEnvFile(env string) Environment {
 	fileName := fmt.Sprintf("env/-%s", env)
+
 	defaultEnvFile, err := godotenv.Read(fileName)
 	if err != nil {
 		dotEnvErr := fmt.Errorf("dot env err: %w", err)
@@ -72,9 +81,9 @@ func readEnvironmentSpecificEnvFile(env string) Environment {
 }
 
 func readCurrentEnv() string {
-	appName, ok := os.LookupEnv(Env)
+	appName, ok := os.LookupEnv(CurrentEnv)
 	if !ok {
-		err := fmt.Errorf("new environment: %w", ErrNoEnv)
+		err := fmt.Errorf("new environment: %w", ErrNocurrentEnv)
 
 		logrus.Error(err)
 		panic(err)
