fb9526c31146e2937ea560c504314e54c9e92c2c
Matthew Slauson <matthew@slauson.io>
2024-01-07 13:27:59 -0600

    good start

--- server/server_test.go
+++ server/server_test.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"fmt"
 	"github.com/slausonio/go-webserver/environment"
+	"github.com/slausonio/go-webserver/server/mocks"
 	"github.com/slausonio/siotest"
 	"net/http"
 	"testing"
@@ -9,24 +11,56 @@ import (
 
 type handler struct{}
 
-var happyEnv = environment.Environment{environment.AppName: "go-webserver",
-	environment.Port: "8080"}
+var HappyEnvMap = environment.SioGoEnv{
+	environment.CurrentEnv: "test",
+	environment.AppName:    "go-webserver",
+	environment.Port:       "8080"}
+
+var CurrentEnvMap = environment.SioGoEnv{"test1": "test", "test2": "test2"}
+
+func EnvSetup(t *testing.T) {
+	t.Helper()
+
+	siotest.CreateFile(t, environment.DefaultFilePath)
+	siotest.CreateFile(t, fmt.Sprintf(environment.CurrentEnvFilePath, "test"))
+
+	siotest.WriteEnvToFile(t, environment.DefaultFilePath, HappyEnvMap)
+	siotest.WriteEnvToFile(t, fmt.Sprintf(environment.CurrentEnvFilePath, "test"), CurrentEnvMap)
+
+}
+
+func EnvCleanup(t *testing.T) {
+	t.Helper()
+
+	t.Cleanup(func() {
+		siotest.RemoveFileAndDirs(t, environment.DefaultFilePath)
+		siotest.RemoveFileAndDirs(t, fmt.Sprintf(environment.CurrentEnvFilePath, "test"))
+	})
+}
+
+func createTestServerStruct(t *testing.T) (*Server, *mocks.Env) {
+	mockEnv := mocks.NewEnv(t)
+
+	return &Server{env: mockEnv}, mockEnv
+
+}
 
 func TestServer_Start(t *testing.T) {
 	t.Run("Happy", func(t *testing.T) {
-		siotest.WriteEnvToFile(t, "env/.env", happyEnv)
-		s := &Server{
-			Environment: environment.NewEnvironment(),
-		}
+		testServer, _ := createTestServerStruct(t)
+
+		//mockEnv.On("Value", environment.Port).Return("test")
+
 		h := http.NewServeMux()
 
-		// This will start a Server which is not ideal but for now we are just ensuring it doesn't crash.
-		go s.Start(h)
+		go testServer.Start(h)
+
+		//t.Cleanup(testServer.Kill)
 	})
 
 	tests := []struct {
 		name        string
-		environment environment.Environment
+		environment environment.SioGoEnv
 	}{
 		{
 			name: "basic",
@@ -35,7 +69,7 @@ func TestServer_Start(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &Server{
-				Environment: environment.NewEnvironment(),
+				env: environment.NewEnvironment(),
 			}
 			h := http.NewServeMux()
 
@@ -45,52 +79,52 @@ func TestServer_Start(t *testing.T) {
 	}
 }
 
-func TestServer_printInfo(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		{"basic"},
-		// TODO Add additional test cases when possible
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &Server{
-				Environment: environment.NewEnvironment(),
-			}
-			s.printInfo(0)
-		})
-	}
-}
-
-func TestServer_printSio(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		{"basic"},
-		// TODO Add additional test cases when possible
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &Server{
-				Environment: environment.NewEnvironment(),
-			}
-			s.printSio()
-		})
-	}
-}
-
-func TestNewServer(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		{"basic"},
-		// TODO Add additional test cases when possible
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewServer(); got == nil {
-				t.Errorf("NewServer() = %v, expected non-nil", got)
-			}
-		})
-	}
-}
+//func TestServer_printInfo(t *testing.T) {
+//	tests := []struct {
+//		name string
+//	}{
+//		{"basic"},
+//		// TODO Add additional test cases when possible
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			s := &Server{
+//				e: environment.NewEnvironment(),
+//			}
+//			s.printInfo(0)
+//		})
+//	}
+//}
+//
+//func TestServer_printSio(t *testing.T) {
+//	tests := []struct {
+//		name string
+//	}{
+//		{"basic"},
+//		// TODO Add additional test cases when possible
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			s := &Server{
+//				e: environment.NewEnvironment(),
+//			}
+//			s.printSio()
+//		})
+//	}
+//}
+//
+//func TestNewServer(t *testing.T) {
+//	tests := []struct {
+//		name string
+//	}{
+//		{"basic"},
+//		// TODO Add additional test cases when possible
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			if got := NewServer(); got == nil {
+//				t.Errorf("NewServer() = %v, expected non-nil", got)
+//			}
+//		})
+//	}
+//}
