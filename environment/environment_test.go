5b0367792bae8b1522fedd0c073428f38da5c5bd
Matthew Slauson <matthew@slauson.io>
2024-01-07 16:25:14 -0600

    finish env tests

---
+++ environment/environment_test.go
@@ -0,0 +1,92 @@
+package environment
+
+import (
+	"fmt"
+	"github.com/slausonio/siotest"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+var currentEnvMap = SioWSEnv{"test1": "test", "test2": "test2"}
+
+var happyEnvMap = SioWSEnv{
+	CurrentEnvKey: "test",
+	AppNameKey:    "go-webserver",
+	PortKey:       "8080",
+}
+
+func EnvSetup(t *testing.T) {
+	t.Helper()
+
+	siotest.CreateFile(t, DefaultFilePath)
+	siotest.CreateFile(t, fmt.Sprintf(CurrentEnvFilePath, "test"))
+
+	siotest.WriteEnvToFile(t, DefaultFilePath, happyEnvMap)
+	siotest.WriteEnvToFile(t, fmt.Sprintf(CurrentEnvFilePath, "test"), currentEnvMap)
+
+}
+
+func EnvCleanup(t *testing.T) {
+	t.Helper()
+
+	t.Cleanup(func() {
+		siotest.RemoveFileAndDirs(t, DefaultFilePath)
+	})
+}
+
+func TestNewEnvironment(t *testing.T) {
+	checkOsFunc := func() {
+		for key, value := range happyEnvMap {
+			os.Getenv(key)
+			assert.Equalf(t, os.Getenv(key), value, "expected %v, got %v", os.Getenv(key), value)
+		}
+	}
+	EnvSetup(t)
+	EnvCleanup(t)
+
+	env := NewEnvironment()
+	assert.Equalf(t, env.Value(CurrentEnvKey), "test", "expected %v, got %v", env.Value(CurrentEnvKey), "test")
+	assert.Equalf(t, env.Value(AppNameKey), "go-webserver", "expected %v, got %v", env.Value(AppNameKey), "go-webserver")
+	assert.Equalf(t, env.Value(PortKey), "8080", "expected %v, got %v", env.Value(PortKey), "8080")
+
+	checkOsFunc()
+}
+
+func TestSioWSEnv_Value(t *testing.T) {
+	tt := []struct {
+		name     string
+		env      SioWSEnv
+		key      string
+		expected string
+	}{
+		{
+			name:     "Existing Key",
+			env:      SioWSEnv{"existingKey": "existingValue"},
+			key:      "existingKey",
+			expected: "existingValue",
+		},
+		{
+			name:     "Non-Existing Key",
+			env:      SioWSEnv{"existingKey": "existingValue"},
+			key:      "nonExistingKey",
+			expected: "",
+		},
+		{
+			name:     "Empty Key",
+			env:      SioWSEnv{"": "emptyKey"},
+			key:      "",
+			expected: "emptyKey",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			value := tc.env.Value(tc.key)
+
+			if value != tc.expected {
+				t.Errorf("expected: %s, got: %s", tc.expected, value)
+			}
+		})
+	}
+}
--- server/server_test.go
+++ server/server_test.go
@@ -1,48 +1,25 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/slausonio/siotest"
 	"github.com/slausonio/siows/environment"
 )
 
-var HappyEnvMap = environment.SioWSEnv{
+var happyEnvMap = environment.SioWSEnv{
 	environment.CurrentEnvKey: "test",
 	environment.AppNameKey:    "go-webserver",
-	environment.PortKey:       "8080"}
-
-var CurrentEnvMap = environment.SioWSEnv{"test1": "test", "test2": "test2"}
-
-func EnvSetup(t *testing.T) {
-	t.Helper()
-
-	siotest.CreateFile(t, environment.DefaultFilePath)
-	siotest.CreateFile(t, fmt.Sprintf(environment.CurrentEnvFilePath, "test"))
-
-	siotest.WriteEnvToFile(t, environment.DefaultFilePath, HappyEnvMap)
-	siotest.WriteEnvToFile(t, fmt.Sprintf(environment.CurrentEnvFilePath, "test"), CurrentEnvMap)
-
-}
-
-func EnvCleanup(t *testing.T) {
-	t.Helper()
-
-	t.Cleanup(func() {
-		siotest.RemoveFileAndDirs(t, environment.DefaultFilePath)
-		siotest.RemoveFileAndDirs(t, fmt.Sprintf(environment.CurrentEnvFilePath, "test"))
-	})
+	environment.PortKey:       "8080",
 }
 
 func TestServer_Start(t *testing.T) {
 	t.Parallel()
 
 	t.Run("Happy", func(t *testing.T) {
-		testServer := NewServer(HappyEnvMap)
+		testServer := NewServer(happyEnvMap)
 		h := http.NewServeMux()
 
 		testServer.Start(h)
@@ -52,7 +29,7 @@ func TestServer_Start(t *testing.T) {
 		t.Skip()
 
 		t.Run("duplicate address", func(t *testing.T) {
-			testServer := NewServer(HappyEnvMap)
+			testServer := NewServer(happyEnvMap)
 
 			h := http.NewServeMux()
 
@@ -67,7 +44,7 @@ func TestServer_Start(t *testing.T) {
 }
 
 func TestServer_getters(t *testing.T) {
-	testServer := NewServer(HappyEnvMap)
+	testServer := NewServer(happyEnvMap)
 
 	t.Run("server", func(t *testing.T) {
 		h := http.NewServeMux()
